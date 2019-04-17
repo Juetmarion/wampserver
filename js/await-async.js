183f7c6609c02b6e33245e4d5822e834ac3aebe0
marion <marion.juet@gmail.com>
2019-04-17 11:10:49 +0200

    async await plusieurs methodes

--- js/await-async.js
+++ js/await-async.js
@@ -1,7 +1,7 @@
 
-  let get = function(url){
+  var get = function(url){
     return new Promise(function(resolve, reject){
-    let req = new XMLHttpRequest();
+    var req = new XMLHttpRequest();
     req.onreadystatechange = function(){
       if (req.readyState === 4){
         if (req.status >= 200 && req.status < 400){
@@ -16,14 +16,40 @@
     })
   }
 
-  let getPosts = async function(){ // 1  on précise le async
-    // on déclare response pour pouvoir faire attendre l'appel
-    let response = await get('https://jsonplaceholder.typicode.com/users')
-    let users = JSON.parse(response)
-    // on attend aussi
-    reponse = await get('https://jsonplaceholder.typicode.com/comments?userId='+ users[0].id)
-    let posts = JSON.parse(response);
+  var getPosts = async function(){ // 1  on précise le async
+    try {
+        // on déclare response pour pouvoir faire attendre l'appel
+        var response = await get('https://jsonplaceholder.typicode.com/users')
+        var users = JSON.parse(response)
+        reponse = await get('https://jsonplaceholder.typicode.com/comments?userId='+ users[0].id)
+        var posts = JSON.parse(response)
+    } catch (e) {
+        console.log('Oups il y a eu un pb', e)
+    }
     return posts
   }
 
-  console.log(getPosts()) // retourne une promesse à cause du async
+  // console.log(getPosts()) // retourne une promesse à cause du async
+
+  var getFirstPosts = async function(){
+      var posts = await getPosts()
+      return posts[0]
+  }
+
+  getFirstPosts().then(function(post){
+      //console.log(post)
+  })
+
+  // faire passer toutes les promesses :
+  // promise.all et on intègre dans un tableau les promesses
+/*   Promise.all([getPosts(), getFirstPosts()]).then(function (arr){
+      console.log(arr)
+  }) */
+
+  var demo = async function(){
+      var arr = await Promise.all([getPosts(), getFirstPosts()])
+      console.log(arr)
+  }
+
+  //demo() // va faire les deux requetes à la fois en simultanée
+  getPosts()
